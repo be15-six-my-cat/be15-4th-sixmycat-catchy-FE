71aebe94ff78f6fdf0174886f5545d3936188529
kimjy8937 <kimjy3520@gmail.com>
2025-05-20 11:42:28 +0900

    feat: CAT-181 FeedHeader.vue 컴포넌트 생성

---
+++ src/features/feed/components/FeedHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="feed-header">
+    <div class="author">
+      <img :src="author.profileImageUrl" alt="profile" class="profile-image" />
+      <span class="nickname">{{ author.nickname }}</span>
+    </div>
+    <span class="text-xs text-gray-400">{{ formattedTime }}</span>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+const props = defineProps({
+  author: Object,
+  createdAt: String,
+});
+const formattedTime = computed(() => dayjs(props.createdAt).fromNow());
+</script>
+
+<style scoped>
+.feed-header {
+  @apply flex items-center justify-between px-4 py-3;
+}
+
+.author {
+  @apply flex items-center gap-3;
+}
+
+.author .profile-image {
+  @apply w-6 h-6 rounded-full object-cover;
+}
+
+.author .nickname {
+  @apply text-xs font-semibold;
+}
+</style>
