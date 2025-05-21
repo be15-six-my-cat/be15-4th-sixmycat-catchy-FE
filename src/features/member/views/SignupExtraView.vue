70c78121ecb6560aaa8a623357b0a351cf7c73e8
didiha <hcring@naver.com>
2025-05-22 03:35:58 +0900

    chore: 입력칸 설명 및 화면 이동 경로 수정

--- src/features/member/views/SignupExtraView.vue
+++ src/features/member/views/SignupExtraView.vue
@@ -20,7 +20,7 @@ const social = urlParams.get('social');
 
 onMounted(async () => {
   if (!email || !social) {
-    alert("잘못된 접근입니다. 이메일 또는 소셜 정보가 누락되었습니다.");
+    alert('잘못된 접근입니다. 이메일 또는 소셜 정보가 누락되었습니다.');
     return;
   }
 
@@ -37,8 +37,8 @@ onMounted(async () => {
       contactReadonly.value = true;
     }
   } catch (err) {
-    console.error("임시 회원 정보 불러오기 실패:", err);
-    alert("임시 회원 정보를 불러오지 못했습니다.");
+    console.error('임시 회원 정보 불러오기 실패:', err);
+    alert('임시 회원 정보를 불러오지 못했습니다.');
   }
 });
 
@@ -49,22 +49,22 @@ const handleImageChange = (e) => {
 const submitSignup = async () => {
   try {
     const formData = new FormData();
-    formData.append("name", name.value);
-    formData.append("contactNumber", contactNumber.value.replace(/-/g, ''));
-    formData.append("nickname", nickname.value);
-    formData.append("email", email);
-    formData.append("social", social.toUpperCase());
+    formData.append('name', name.value);
+    formData.append('contactNumber', contactNumber.value.replace(/-/g, ''));
+    formData.append('nickname', nickname.value);
+    formData.append('email', email);
+    formData.append('social', social.toUpperCase());
     if (profileImage.value) {
-      formData.append("profileImage", profileImage.value);
+      formData.append('profileImage', profileImage.value);
     }
 
     const { data } = await socialSignupExtra(formData);
-    alert("회원가입이 완료되었습니다!");
-    router.push('/');
+    alert('회원가입이 완료되었습니다!');
+    router.push('/member/start');
   } catch (error) {
     const { errorCode, message } = error.response?.data ?? {};
     alert(`[${errorCode ?? 'ERROR'}] ${message ?? '알 수 없는 오류가 발생했습니다.'}`);
-    console.error("회원가입 실패:", error);
+    console.error('회원가입 실패:', error);
   }
 };
 </script>
@@ -80,9 +80,19 @@ const submitSignup = async () => {
       />
     </div>
     <div class="inputs-frame">
-      <Input title="이름" text="이름을 입력해주세요" v-model="name" :readonly="nameReadonly" />
-      <Input title="전화번호" text="전화번호를 입력해주세요" v-model="contactNumber" :readonly="contactReadonly" />
-      <Input title="닉네임" text="영어 및 특수문자만 입력 가능합니다" v-model="nickname" />
+      <Input
+        title="이름"
+        text="이름을 입력해주세요 (후에 수정 불가)"
+        v-model="name"
+        :readonly="nameReadonly"
+      />
+      <Input
+        title="전화번호"
+        text="전화번호를 입력해주세요 (후에 수정 불가)"
+        v-model="contactNumber"
+        :readonly="contactReadonly"
+      />
+      <Input title="닉네임" text="영어 및 특수문자(., _, ^)만 입력 가능합니다" v-model="nickname" />
       <input type="file" accept="image/*" @change="handleImageChange" />
     </div>
   </div>
