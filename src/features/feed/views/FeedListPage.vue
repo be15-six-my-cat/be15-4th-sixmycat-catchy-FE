b3c7f1729175df554e56d448bbfacb539213fe4e
sezeme <stellaa223@gmail.com>
2025-05-22 14:17:14 +0900

    refactor : 임시 데이터 삭제

--- src/features/feed/views/FeedListPage.vue
+++ src/features/feed/views/FeedListPage.vue
@@ -50,14 +50,11 @@ onMounted(async () => {
 onBeforeUnmount(() => {
   if (observer.value) observer.value.disconnect();
 });
-
-const selectedTab = ref('MyFeed');
 </script>
 
 <template>
   <div class="flex flex-col gap-6 items-center p-6">
     <router-view />
-    <MyThumbnailList :selectedTab="selectedTab" />
     <FeedCard v-for="feed in feeds" :key="feed.id" :feed="feed" />
     <p v-if="isLoading" class="loading">불러오는 중...</p>
     <p v-if="!isLoading && feeds.length === 0" class="empty">피드가 없습니다.</p>
