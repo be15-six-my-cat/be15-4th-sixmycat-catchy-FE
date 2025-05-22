5ef76de961f5554eba14b3d31c78b4454eb424f0
kimjy8937 <kimjy3520@gmail.com>
2025-05-22 19:30:40 +0900

    feat: CAT-230 api 호출 메서드 수정

--- src/api/feed.js
+++ src/api/feed.js
@@ -2,44 +2,23 @@ import api from './axios.js';
 
 export const uploadImages = (data) => api.post('/feed/images', data);
 
-export const createFeed = (data) =>
-  api.post('/feeds', data, {
-    headers: {
-      'X-USER-ID': 1,
-    },
-  });
+export const createFeed = (data) => api.post('/feeds', data);
 
 export function fetchFeedList(page = 0, size = 2) {
-  return api.get('/feeds', {
-    params: { page, size },
-  });
+  return api.get('/feeds');
 }
 
-export const fetchFeed = (feedId) =>
-  api.get(`/feeds/${feedId}`, {
-    headers: {
-      'X-USER-ID': 1,
-    },
-  });
+export const fetchFeed = (feedId) => api.get(`/feeds/${feedId}`);
 
-export const editFeed = (feedId, data) =>
-  api.put(`/feeds/${feedId}`, data, {
-    headers: { 'X-USER-ID': 1 },
-  });
+export const editFeed = (feedId, data) => api.put(`/feeds/${feedId}`, data);
 
-export const deleteFeed = (feedId) =>
-  api.delete(`/feeds/${feedId}`, {
-    headers: { 'X-USER-ID': 1 },
-  });
+export const deleteFeed = (feedId) => api.delete(`/feeds/${feedId}`);
 
 /* 내가 등록한 피드 썸네일 목록 조회 */
 export const fetchMyFeedList = (params = { page: 0, size: 9 }) => {
   params.page = params.page - 1;
   return api.get('/feeds/me', {
     params,
-    headers: {
-      'X-USER-ID': 1,
-    },
   });
 };
 
@@ -50,8 +29,5 @@ export const fetchLikedFeedList = (params = { page: 0, size: 9 }) => {
   params.page = params.page - 1;
   return api.get('/feeds/likes', {
     params,
-    headers: {
-      'X-USER-ID': 1,
-    },
   });
 };
--- src/api/like.js
+++ src/api/like.js
@@ -1,16 +1,8 @@
 import api from './axios.js';
 
-export const likeFeed = (data) =>
-  api.post('/likes', data, {
-    headers: {
-      'X-USER-ID': 1,
-    },
-  });
+export const likeFeed = (data) => api.post('/likes', data);
 
 export const unLikeFeed = (data) =>
   api.delete('/likes', {
     data,
-    headers: {
-      'X-USER-ID': 1,
-    },
   });
