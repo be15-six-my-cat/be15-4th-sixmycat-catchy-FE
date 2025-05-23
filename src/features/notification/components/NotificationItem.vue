250624ded3e4cb85bf66abb17d69c0aa2969a984
sezeme <stellaa223@gmail.com>
2025-05-23 21:47:54 +0900

    feat: 알림 코멘트 수정

--- src/features/notification/components/NotificationItem.vue
+++ src/features/notification/components/NotificationItem.vue
@@ -44,18 +44,20 @@ const timeAgo = computed(() => {
 
 const notificationText = computed(() => {
   const type = props.notification.type;
-  const base = `님이 회원님을 `;
+  const base = `님이 회원님`;
   switch (type) {
     case 'FOLLOW':
-      return `${base}팔로우하기 시작했습니다.`;
+      return `${base}을 팔로우하기 시작했습니다.`;
     case 'COMMENT':
-      return `${base}게시물에 댓글을 남겼습니다.`;
+      return `${base}의 게시물에 댓글을 남겼습니다.`;
     case 'RECOMMENT':
-      return `${base}댓글에 답글을 남겼습니다.`;
-    case 'LIKE':
-      return `${base}피드/쭈르을(를) 좋아합니다.`;
+      return `${base}의 댓글에 답글을 남겼습니다.`;
+    case 'FEED_LIKE':
+      return `${base}의 피드를 좋아합니다.`;
+    case 'JJURE_LIKE':
+      return `${base}의 쭈르를 좋아합니다.`;
     case 'BIRTHDAY':
-      return `님의 냥이 생일 축하해요! 🐾🎂🐱🎉`;
+      return `회원님의 냥이 생일 축하해요! 🐾🎂🐱🎉`;
     default:
       return '';
   }
